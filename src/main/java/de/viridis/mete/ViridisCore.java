19e55ea2ccc94d2b3a722aff222acab7fe1ea38f
Mete74-132 <geomete74@gmail.com>
2026-04-04 18:25:39 +0200

    dok spielt mehr fortnite als  i cant found you

---
+++ src/main/java/de/viridis/mete/ViridisCore.java
@@ -0,0 +1,16 @@
+package de.viridis.mete;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ViridisCore implements ModInitializer {
+	public static final String MOD_ID = "viridiscore";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/de/viridis/mete/ViridisCoreClient.java
@@ -0,0 +1,10 @@
+package de.viridis.mete;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ViridisCoreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
